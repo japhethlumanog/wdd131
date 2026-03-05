b500fdfb0e67f73ab76251c281db6220aa35427d
japhethlumanog3@gmail.com <japhethlumanog3@gmail.com>
2026-03-05 10:06:30 +0800

    week1 project

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,8 @@
+const today = new Date();
+
+const options = { year: 'numeric', month: 'long', day: 'numeric' };
+const formattedDate = today.toLocaleDateString('en-US', options);
+
+document.getElementById("currentyear").textContent = formattedDate;
+
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
